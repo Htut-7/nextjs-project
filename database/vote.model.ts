10ca041472e4ba58e8edf8f64926340d76691d70
Champ is God <hsithuhtut@gmail.com>
2026-08-03 10:27:47 +0700

    feat:(database) add vote model

---
+++ database/vote.model.ts
@@ -0,0 +1,40 @@
+import { Schema, Types, Document, models, model } from "mongoose";
+
+export interface Ivote {
+  author: Types.ObjectId;
+  type_id: Types.ObjectId;
+  type: string;
+  votetype: string;
+}
+
+export interface IvoteDoc extends Ivote, Document {}
+
+const voteSchema = new Schema(
+  {
+    author: {
+      type: Schema.Types.ObjectId,
+      require: true,
+      ref: "Users",
+    },
+    type_id: {
+      type: Schema.Types.ObjectId,
+      require: true,
+    },
+    type: {
+      type: String,
+      require: true,
+      enum: ["question", "answer"],
+    },
+    votetype: {
+      type: String,
+      require: true,
+      enum: ["upvote", "downvote"],
+    },
+  },
+  { timestamps: true }
+);
+
+voteSchema.index({ author: 1, type_id: 1 }, { unique: true });
+
+const Vote = models?.Votes || model<Ivote>("Votes", voteSchema);
+export default Vote;
