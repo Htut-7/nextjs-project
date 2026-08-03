9730836cf7ecc89bcbe3d931448f67605c786dab
Champ is God <hsithuhtut@gmail.com>
2026-08-03 10:39:38 +0700

    feat: (database) add interaction schema

---
+++ database/interaction.model.ts
@@ -0,0 +1,38 @@
+import { Schema, Types, Document, model, models } from "mongoose";
+
+export interface Iinteraction {
+  user: Types.ObjectId;
+  action: string;
+  action_id: Types.ObjectId;
+  actiontype: string;
+}
+
+export interface IinteractionDoc extends Iinteraction, Document {}
+
+const interactionSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Users",
+    },
+    action: {
+      type: String,
+      required: true,
+    },
+    action_id: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    actiontype: {
+      type: String,
+      required: true,
+      enum: ["question", "answer"],
+    },
+  },
+  { timestamps: true }
+);
+
+const Interaction =
+  models?.Interaction || model<Iinteraction>("Interaction", interactionSchema);
+export default Interaction;
