1206bbbb5f64b0f5e4af3f753a7b5fbb5544b981
Champ is God <hsithuhtut@gmail.com>
2026-08-24 20:00:55 +0700

    fix navbar avatar and logout

--- Components/LeftSidebar.tsx
+++ Components/LeftSidebar.tsx
@@ -3,7 +3,13 @@ import Link from "next/link";
 import { FaHome, FaSignOutAlt } from "react-icons/fa";
 import ROUTES from "@/ROUTES";
 
-function LeftSidebar() {
+import { auth, signOut } from "@/auth";
+import { redirect } from "next/navigation";
+
+async function LeftSidebar() {
+  let session = await auth();
+  let user = session?.user;
+
   return (
     <div className="w-1/5 px-5 py-2">
       <ul className="space-y-6">
@@ -47,15 +53,38 @@ function LeftSidebar() {
           </Link>
         </li>
 
-        <li className="bg-red-500 px-3 py-3 rounded-xl">
-          <Link
-            href={ROUTES.QUESTIONS}
-            className="text-[16px] font-bold flex items-center space-x-3.5"
-          >
-            <FaSignOutAlt />
-            <span>Logout</span>
-          </Link>
-        </li>
+        {!user && (
+          <li className=" px-3 py-3 rounded-xl border-2 border-main">
+            <Link
+              href={ROUTES.LOGIN}
+              className="text-[16px] font-bold flex items-center space-x-3.5"
+            >
+              <FaHome />
+              <span>Login</span>
+            </Link>
+          </li>
+        )}
+
+        {user && (
+          <li className="bg-red-500 px-3 py-3 rounded-xl">
+            <form
+              action={async () => {
+                "use server";
+
+                await signOut({ redirect: false });
+                redirect(ROUTES.LOGIN);
+              }}
+            >
+              <button
+                type="submit"
+                className="text-[16px] font-bold flex items-center space-x-3.5"
+              >
+                <FaSignOutAlt />
+                <span>Logout</span>
+              </button>
+            </form>
+          </li>
+        )}
       </ul>
     </div>
   );
--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Image from "next/image";
 import logo from "../public/logo1.jpg";
-import Profile from "../public/Profile.jpg";
-import Input from "../Components/Input";
 import SearchInput from "./SearchInput";
+import { auth } from "@/auth";
 
-function page() {
+async function page() {
+  let session = await auth();
+  let user = session?.user;
+
   return (
     <nav className="flex justify-between px-10 py-6">
       <div className="flex items-center justify-center space-x-4">
@@ -24,13 +26,15 @@ function page() {
         <SearchInput />
       </div>
       <div>
-        <Image
-          src={Profile}
-          alt="logo"
-          className="aspect-square rounded-full object-cover"
-          width={45}
-          height={45}
-        />
+        {user && (
+          <Image
+            src={user?.image || "default-avatar.png"}
+            alt="logo"
+            className="aspect-square rounded-full object-cover"
+            width={45}
+            height={45}
+          />
+        )}
       </div>
     </nav>
   );
